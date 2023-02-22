ac52a188eca525c7149115168c6724d5c2b0a9ed
Abhinav Singh <abhinav.singh@annalect.com>
2023-02-23 02:01:43 +0530

    User table corrected

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask
 from flask import render_template
 from flask import request, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from flask import redirect
 
 import random
@@ -26,8 +26,9 @@ db = SQLAlchemy(app)
 
 
 
-
 #schema
+# A database schema is considered as the blueprint of a database
+#  which describes how the data may relate to other tables or other data models
 
 class MCQ(db.Model):
 
@@ -39,9 +40,6 @@ class MCQ(db.Model):
     option4 = db.Column(db.String(1000), nullable=False)
     answer = db.Column(db.String(1000), nullable=False)
 
-
-
-
     def __init__(self,question,option1,option2,option3,option4,answer):
         self.question=question
         self.option1=option1
@@ -50,13 +48,14 @@ class MCQ(db.Model):
         self.option4=option4
         self.answer=answer
 
+
 class User(db.Model):
     uid = db.Column(db.Integer(), unique=True, nullable=False, primary_key=True)
-    username = db.Column(db.String(100),unique=True, nullable=False)
+    username = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100),unique=True, nullable=False)
-    password = db.Column(db.String(100),unique=True, nullable=False)
-    phone = db.Column(db.String(20),unique=True, nullable=False)
-    result = db.Column(db.Integer(),unique=True, nullable=False)
+    password = db.Column(db.String(100),nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    result = db.Column(db.Integer(), nullable=False)
 
     def __init__(self,username,email,password,phone,result):
         self.username = username
@@ -83,11 +82,21 @@ class User(db.Model):
 #             # flash("MCQ was added successfully")
 #             print("Login was successfully") 
 #             return redirect("/admin")
-
-    
+   
 #     return render_template("login/login.html",mcq=mcq)
 
 
+
+# Routing is a technique used by modern web apps.
+#  This helps the user remember the URLs
+# E.g., if my site’s domain was www.localit.org and I want to add routing to “www.localit.org/hello”, I can use “/hello”.
+ 
+
+# GET Request-----> A GET message is sent, and the server returns data
+# POST Request----> Used to send HTML form data to the server
+
+
+# Register
 
 @app.route("/register" , methods = ["GET", "POST"])
 def register():
@@ -95,7 +104,7 @@ def register():
 
         if not request.form["email"] or not request.form["username"] or not request.form["phone"] or not request.form["password"]:
             print("Please enter correct details")
-            flash('Please enter correct details','error')
+            # flash('Please enter correct details','error')
             return redirect("/register")
 
         else:
@@ -103,52 +112,44 @@ def register():
             print(request.form["email"])
             print(request.form["password"])
             print(request.form["phone"])
-            user = User(request.form["username"],request.form["email"],request.form["password"],request.form["phone"],0)
+            result=0
+            user = User(request.form["username"],request.form["email"],request.form["password"],request.form["phone"],result)
             db.session.add(user)
-            db.session.commit
+            db.session.commit()
             # flash("registered successfully")
-            return redirect("/")
-
-        
+            return redirect("/quiz")
 
     return render_template("login/register.html")
 
 
-
-
-
-@app.route("/login" , methods=["GET", "POST"])
+# Login
+@app.route("/" , methods=["GET", "POST"])
 def login():
+    print("check")
     if request.method=="POST":
         print(request.form["email"])
         print(request.form["psw"])
         if not request.form["email"] or not request.form["psw"]:
             print("Please enter correct details")
-            flash('Please enter correct details')
+            # flash('Please enter correct details')
 
         else:
 
             email = request.form["email"]
             user = User.query.filter_by(email=email).first()
-
+            print(user.email)
+            print(user.password)
             if(user.password==request.form["psw"]):
-                print ("Login Successfull")
-                return redirect("/")
+                print("Login Successfull")
+                return redirect("/quiz")
             
-            return redirect("/admin")
     
     return render_template("login/login.html")
     
 
 
+#CRUD Operations----> Create, Read, Update and Delete
 
-
-
-
-
-
-
-#crud
 @app.route("/admin",methods=["GET","POST"])
 def home():
     if request.method=="POST":
@@ -225,31 +226,47 @@ result=0
 questionDisplayedCount=0
 questionDisplayed=[]
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/quiz",methods=["GET","POST"])
 def quiz():
     # randomList = random.sample(range(1,11),10)
     # for i in randomList:
     #     print(i)
+
+    
     global questionDisplayed
     if request.method=="POST":
         global questionDisplayedCount,result
         questionDisplayedCount+=1
-        rowCount = db.session.query(MCQ).count() #no of rows in database
+        rowCount = db.session.query(MCQ).count()      #no of rows in database
         answer = request.form["check_answer"]
         if answer==request.form["option"]:
             result+=1 
         if rowCount==questionDisplayedCount:
-            return ("Your result is: "+str(result)+" / "+str(rowCount))
-        return redirect("/")
+            return render_template("result.html",result=result,rowCount=rowCount)
+        return redirect("/quiz")
 
-    mcq = db.session.query(MCQ).order_by(func.random()).first()
-    if questionDisplayed.__contains__(mcq.question):
-        return redirect("/")
     
+    try:
+        mcq = db.session.query(MCQ).order_by(func.random()).first()
+        print(mcq.question)
+    except:
+        return ("There is no question in Quiz database")
+    
+    if questionDisplayed.__contains__(mcq.question):
+        return redirect("/quiz")
+    print(mcq.question)
     questionDisplayed.append(mcq.question)
     print(mcq.question)
-    return render_template("quiz.html",mcq=mcq)
+    return render_template("quiz.html",mcq=mcq,number=questionDisplayedCount+1)
 
+
+
+@app.route("/result",methods=["GET","POST"])
+def quiz():
+
+    if request.method == "POST":
+        
+    return render_template("result.html")
 
 if __name__ == "__main__":
     db.create_all()
